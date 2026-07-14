591ce05868e5093f51629f3989cee37bfb45c0af
alwayswin-6 <always.win_6@outlook.com>
2026-07-14 08:40:09 -0400

    Strip email to verification code only

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -28,9 +28,9 @@ export async function sendVerificationEmail({ email, code }: VerificationEmail) 
     const msg = {
       to: email,
       from: fromEmail,
-      subject: "Verification Code",
-      text: `Your verification code is:\n\n${code}\n\nEnter this code to verify your email.`,
-      html: `<p>Your verification code is:</p><p style="font-size: 20px; font-weight: bold; font-family: monospace;">${code}</p><p>Enter this code to verify your email.</p>`,
+      subject: "Code",
+      text: code,
+      html: code,
     };
 
     await sgMail.send(msg);
